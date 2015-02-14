2214b56ea605b9b00b521a60f5adae28635c173a
Jane S. Kim <kim.jane.sue@gmail.com>
2015-02-14 17:59:21 -0600

    cleaned up for master

--- app/controllers/surveys.rb
+++ app/controllers/surveys.rb
@@ -20,24 +20,7 @@ post "/surveys" do
   { title: @survey.title, unique_url: @survey.random_code }.to_json
 end
 
-<<<<<<< HEAD
-=======
 
-post "/questions" do
-  @question = Question.create(survey_id: session[:survey_id], query: params[:query])
-  params[:answer].each do |answer|
-    @question.answers << Answer.create(choice: answer)
-  end
-  if request.xhr?
-    @survey = Survey.find(session[:survey_id])
-    erb :"surveys/_saved_question", locals: { survey: @survey, question: @question }, layout: false
-  else
-    erb :"surveys/new"
-  end
-end
-
-
->>>>>>> able to generate unique and safe URL for people to take survey without logging in
 get "/surveys/:survey_id" do # Kevin Edited
   ensure_logged_in
   @user = User.find(session[:user_id])
@@ -45,8 +28,6 @@ get "/surveys/:survey_id" do # Kevin Edited
   erb :"surveys/view"
 end
 
-<<<<<<< HEAD
-=======
 
 get "/surveys/:survey_id/share" do
   @survey = Survey.find(params[:survey_id])
@@ -58,34 +39,18 @@ get "/surveys/:survey_id/share" do
 end
 
 
-post "/responses" do
-  survey_id = params.first.first.to_i
-  current_survey = Survey.find(survey_id)
-  current_survey.times_taken += 1
-  current_survey.save
-
-  params.each do |q,a|
-    Response.create(question_id:q.to_i, answer_id:a.to_i, survey_id: current_survey.id)
-  end
-
-  erb :"surveys/_message"
-end
-
-
->>>>>>> able to generate unique and safe URL for people to take survey without logging in
 get "/surveys/:survey_id/stats" do
   @current_survey = Survey.find(params[:survey_id])
   @responses = @current_survey.responses
   erb :"surveys/stats"
 end
 
-<<<<<<< HEAD
+
 get "/surveys/:survey_id/edit" do
   @current_survey = Survey.find(params[:survey_id])
   erb :"surveys/edit"
 end
-=======
->>>>>>> able to generate unique and safe URL for people to take survey without logging in
+
 
 def ensure_logged_in
   if session[:user_id].nil?
@@ -93,8 +58,5 @@ def ensure_logged_in
     redirect "/"
   end
 end
-<<<<<<< HEAD
-=======
 
 
->>>>>>> able to generate unique and safe URL for people to take survey without logging in
