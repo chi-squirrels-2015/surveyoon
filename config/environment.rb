83bbecbab9c59c8a70663fbcb1540ac273806a1f
Jane S. Kim <kim.jane.sue@gmail.com>
2015-02-14 21:08:00 -0600

    added CSS

--- config/environment.rb
+++ config/environment.rb
@@ -20,11 +20,8 @@ require "sinatra/reloader" if development?
 
 require 'bcrypt'
 
-<<<<<<< HEAD
 require 'securerandom'
 
-=======
->>>>>>> created models, migration, associations, and seed
 require 'erb'
 
 # Some helper constants for path-centric logic
