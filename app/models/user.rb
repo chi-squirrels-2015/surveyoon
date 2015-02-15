0d5b2b8c3fe32989ab4afb69d28ac4ba3a9a2e63
Jane S. Kim <kim.jane.sue@gmail.com>
2015-02-14 20:54:38 -0600

    updating my files to match master

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,8 @@ class User < ActiveRecord::Base
   has_many :questions, through: :surveys
   has_many :answers, through: :questions
 
-<<<<<<< HEAD
   validates :username, uniqueness: true
-=======
   validates :email, uniqueness: true
->>>>>>> created models, migration, associations, and seed
 
   def password
     BCrypt::Password.new(self.password_hash)
@@ -17,14 +14,8 @@ class User < ActiveRecord::Base
     self.password_hash = BCrypt::Password.create(input)
   end
 
-<<<<<<< HEAD
-
   def self.authenticate(username, password_input)
     user = User.find_by(username: username)
-=======
-  def self.authenticate(email, password_input)
-    user = User.find_by_email(email)
->>>>>>> created models, migration, associations, and seed
     if user
       return user if user && user.password == password_input
     end
