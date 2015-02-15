3ab24a973a12fd16ed08a4a2e2256805c5a2fe5d
Kevin Yoon <KevinY105@gmail.com>
2015-02-14 18:14:08 -0600

    most updated version of master: everything is working, need to update CSS

--- db/migrate/20150213143454_create_surveys.rb
+++ db/migrate/20150213143454_create_surveys.rb
@@ -2,14 +2,8 @@ class CreateSurveys < ActiveRecord::Migration
   def change
     create_table :surveys do |t|
       t.string :title
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
       t.string :random_code
->>>>>>> able to generate unique and safe URL for people to take survey without logging in
       t.integer :times_taken, default: 0
-=======
->>>>>>> created models, migration, associations, and seed
       t.belongs_to :creator
 
       t.timestamps
